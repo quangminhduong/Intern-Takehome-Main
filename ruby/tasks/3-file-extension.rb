aeb7f510b400167d9ee07ea0b83af808043eb87d
Duong Quang Minh <s3759757@student.rmit.edu.au>
2022-03-05 01:29:27 +1100

    Task 3 completed

--- ruby/tasks/3-file-extension.rb
+++ ruby/tasks/3-file-extension.rb
@@ -6,5 +6,11 @@
  # @returns {String} The file extension (with no period), or false if there is none.
 #
 def get_file_extension(filename)
-
+    # search for the period symbol in the string
+    if filename.include?('.')
+        #return the string behind that symbol
+        return filename.partition('.').last
+    else
+        return false
+    end
 end
