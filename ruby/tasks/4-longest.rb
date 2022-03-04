2a943df9c4f8bcfa869fff3b5c8b3ed1bdb66f6b
Duong Quang Minh <s3759757@student.rmit.edu.au>
2022-03-05 01:57:18 +1100

    Task 4 completed

--- ruby/tasks/4-longest.rb
+++ ruby/tasks/4-longest.rb
@@ -6,5 +6,14 @@
  # @returns {String} The longest string from within the input array.
 #
 def longest_string(strings)
-
+    # create an empty array that only store element of type string
+    array_of_string = Array.new
+    for elements in strings
+        if elements.instance_of? String
+            #add the string to the new array as detected
+            array_of_string.push(elements)
+        end
+    end
+    # now we can return the actual longest string from the input array
+    return array_of_string.max_by(&:length)
 end
