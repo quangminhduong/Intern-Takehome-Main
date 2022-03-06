e7030b90a6d3db1dd3d4463e9b78d429d6958784
Duong Quang Minh <s3759757@student.rmit.edu.au>
2022-03-07 01:53:57 +1100

    Task 5 completed

--- ruby/tasks/5-array-sum.rb
+++ ruby/tasks/5-array-sum.rb
@@ -6,6 +6,13 @@
  # @returns {Number} The sum of all integers contained in the input, at any level.
 #
 def array_sum(start_of_tree)
-
+    sum_of_array = 0;
+    # turn the nested array into a 1-d array to make it simple
+    for elements in start_of_tree.flatten
+        #add it to the sum as an integer is detected
+        if elements.instance_of? Integer
+            sum_of_array+= elements
+        end
+    end
+    return sum_of_array
 end
-
